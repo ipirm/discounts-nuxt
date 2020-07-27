97a9452e48e7bf5a4b88a7535bf3d958578b2d18
Ilham <ilham.pirm@gmail.com>
2020-07-27 18:37:34 +0400

    Contact Form

---
+++ components/elements/ContactForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <form class="contact-form">
+    <p class="contact-form-title">Напишите нам</p>
+    <span class="contact-form-text">Мы с вами сами свяжемся по любому вопросу</span>
+    <input class="contact-form-name" type="text" placeholder="Ваше имя">
+    <input class="contact-form-email" type="text" placeholder="Ваш e-mail">
+    <textarea class="contact-form-textarea" placeholder="Ваше сообщение..."/>
+    <a class="contact-form-btn">
+      <span>Отправить</span>
+    </a>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: "ContactForm"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/information/_slug.vue
+++ pages/information/_slug.vue
@@ -9,20 +9,26 @@
         </div>
       </div>
     </div>
-    <div class="container">
+    <div class="container-fluid">
       <div class="row">
-        <div class="col-12">
-          <div v-for="item in page.content" :key="item.id">
-            <div class="about-country" v-if="item.bordered === '0'">{{ item.quoto[$i18n.locale]}}</div>
-            <div class="about-title" v-if="item.bordered === '0'">{{ item.title[$i18n.locale]}}</div>
-            <div class="about-content" v-if="item.bordered === '0'" v-html="item.text[$i18n.locale]"></div>
-            <div class="about-mision" v-else>
-              <div class="about-mision-content">
-                <p>{{ item.title[$i18n.locale]}}</p>
-                <div v-html="item.text[$i18n.locale]"></div>
+        <div class="col-lg-2"></div>
+        <div class="col-lg-6">
+          <div class="container-information">
+            <div v-for="item in page.content" :key="item.id">
+              <div class="about-country" v-if="item.bordered === '0'">{{ item.quoto[$i18n.locale]}}</div>
+              <div class="about-title" v-if="item.bordered === '0'">{{ item.title[$i18n.locale]}}</div>
+              <div class="about-content" v-if="item.bordered === '0'" v-html="item.text[$i18n.locale]"></div>
+              <div class="about-mision" v-else>
+                <div class="about-mision-content">
+                  <p>{{ item.title[$i18n.locale]}}</p>
+                  <div v-html="item.text[$i18n.locale]"></div>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+        <div class="col-lg-2">
+          <ContactForm/>
         </div>
       </div>
     </div>
@@ -31,14 +37,16 @@
 
 <script>
   import {mapState} from 'vuex'
+  import ContactForm from "../../components/elements/ContactForm";
 
   export default {
+    components: {ContactForm},
     async fetch({store, route}) {
       await store.dispatch('pages/getPageBySlug', route.params.slug)
     },
     computed: {
       ...mapState('pages', ['page']),
-    }
+    },
   }
 </script>
 
